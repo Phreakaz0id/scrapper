bcc19ef2876ad8b0e717c66ae56c78277e8bbf8d
Santiago <santiago@camba.coop>
2021-08-10 18:54:14 -0300

    Implements a YaguarItem class

--- scrapper/items.py
+++ scrapper/items.py
@@ -18,3 +18,10 @@ class MaxiconsumoItem(Item):
     product_url = Field()
     bundle_price = Field()
     unit_price = Field()
+
+
+class YaguarItem(Item):
+    category = Field()
+    code = Field()
+    product_name = Field()
+    price = Field()
