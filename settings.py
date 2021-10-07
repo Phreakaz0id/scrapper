48f770b1c2d671a4462d43768c1e97ab9c558011
Wasabi <makesmereal@gmail.com>
2021-10-07 14:45:55 -0300

    :wrench: Adds logging and json file settings

--- settings.py
+++ settings.py
@@ -1,4 +1,10 @@
 import os
 
+from datetime import datetime
+
 OUTPUT_DIR = os.environ["OUTPUT_DIR"]
 LOGS_DIR = os.environ["LOGS_DIR"]
+JOBS_JSON_FILE = os.environ["JOBS_JSON_FILE"]
+
+EXECUTION_TIMESTAMP = datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
+MAIN_LOG_FILE = f"{LOGS_DIR}/{EXECUTION_TIMESTAMP}_main.log"
