129d9c408bd00f26b86b3e87da15305af35de094
Wasabi <makesmereal@gmail.com>
2021-10-07 11:39:40 -0300

    :computer: Updates main.py to use a ScrapyRunner

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# import os
+import os
 
 from datetime import datetime
 from twisted.internet import reactor
@@ -6,6 +6,13 @@ from scrapy.crawler import CrawlerRunner, CrawlerProcess
 from scrapper.spiders.maxiconsumo import MaxiconsumoSpider
 from scrapper.spiders.yaguar import YaguarSpider
 
+from scrapy.utils.project import get_project_settings
+
+os.environ['SCRAPY_SETTINGS_MODULE'] = 'scrapper.settings'
+scrapy_settings = get_project_settings()
+scrapy_settings.set('ITEM_PIPELINES', {
+    'scrapper.pipelines.MultiCSVItemPipeline': 300,
+})
 
 OUTPUT_PATH = 'output'
 
@@ -64,25 +71,33 @@ if __name__ == "__main__":
         # }
     ]
 
-    for job in jobs:
-        feed_format = job["settings"]["feed_format"]
-        job_type = job["settings"]["type"]
-        category = job["args"]["category"]
+    # for job in jobs:
+    #     feed_format = job["settings"]["feed_format"]
+    #     job_type = job["settings"]["type"]
+    #     category = job["args"]["category"]
 
-        args = job["args"]
+    #     args = job["args"]
 
-        process = CrawlerProcess({
-            # 'DOWNLOAD_DELAY': 0.25,
-            'DOWNLOAD_DELAY': 0,
-            'FEED_URI': f"{OUTPUT_PATH}/{now}_{job_type}_{category}.csv",
-            # 'FEED_URI': f"{now}_{job_type}_{category}.csv",
-            'FEED_FORMAT': feed_format
-        })
-        spider = get_spider_by_type(job_type)
-        process.crawl(
-            spider,
-            **args
-        )
-        # d = runner.join()
-        # d.addBoth(lambda _: reactor.stop())
-        process.start()
+    #     process = CrawlerProcess({
+    #         # 'DOWNLOAD_DELAY': 0.25,
+    #         'DOWNLOAD_DELAY': 0,
+    #         'FEED_URI': f"{OUTPUT_PATH}/{now}_{job_type}_{category}.csv",
+    #         # 'FEED_URI': f"{now}_{job_type}_{category}.csv",
+    #         'FEED_FORMAT': feed_format
+    #     })
+    #     spider = get_spider_by_type(job_type)
+    #     process.crawl(
+    #         spider,
+    #         **args
+    #     )
+    #     # d = runner.join()
+    #     # d.addBoth(lambda _: reactor.stop())
+    #     process.start()
+
+    runner = CrawlerRunner(settings=scrapy_settings)
+    runner.crawl(MaxiconsumoSpider, category="limpieza", max_pages=1)
+    d = runner.join()
+    d.addBoth(lambda _: reactor.stop())
+
+    # the script will block here until the crawling is finished
+    reactor.run()
