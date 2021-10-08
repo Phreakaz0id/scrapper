2dce23e3ec984ba7eaae9cda454f31fbd0b7e0c1
Wasabi <makesmereal@gmail.com>
2021-10-07 23:06:33 -0300

    :computer: logs errors on main script

--- main.py
+++ main.py
@@ -8,12 +8,20 @@ from settings import MAIN_LOG_FILE
 from twisted.internet import reactor
 
 
+def main():
+    try:
+        # Initializing log file
+        logfile(MAIN_LOG_FILE)
+        logger.info("🏃‍♀️ [Main] Script starts and will: fetch jobs, run jobs.")
+        # Fetches jobs
+        jobs = fetch_jobs()
+        # Runs jobs
+        run_jobs(jobs)
+        reactor.run()
+    except Exception as e:
+        logfile(MAIN_LOG_FILE)
+        logger.error(e, exc_info=True)
+
+
 if __name__ == "__main__":
-    # Initializing log file
-    logfile(MAIN_LOG_FILE)
-    logger.info("🏃‍♀️ [Main] Script starts and will: fetch jobs, run jobs.")
-    # Fetches jobs
-    jobs = fetch_jobs()
-    # Runs jobs
-    run_jobs(jobs)
-    reactor.run()
+    main()
